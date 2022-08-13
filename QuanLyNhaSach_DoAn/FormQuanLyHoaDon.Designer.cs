81a99edac6a8664033c6235aa467cdebd3d86a15
Hồ Tấn Tài <77793889+hotantai181@users.noreply.github.com>
2022-08-13 14:01:55 +0700

    first commit

---
+++ QuanLyNhaSach_DoAn/FormQuanLyHoaDon.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace QuanLyNhaSach_DoAn
+{
+    partial class FormQuanLyHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormQuanLyHoaDon));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnthoat = new System.Windows.Forms.ToolStripButton();
+            this.lbltenuser = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.datadshd = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbltonghd = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbldoanhthu = new System.Windows.Forms.Label();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblhdchon = new System.Windows.Forms.Label();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datadshd)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnthoat,
+            this.lbltenuser,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(816, 27);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnthoat.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthoat.Image = ((System.Drawing.Image)(resources.GetObject("btnthoat.Image")));
+            this.btnthoat.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(83, 24);
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // lbltenuser
+            // 
+            this.lbltenuser.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.lbltenuser.ForeColor = System.Drawing.Color.Maroon;
+            this.lbltenuser.Name = "lbltenuser";
+            this.lbltenuser.Size = new System.Drawing.Size(24, 24);
+            this.lbltenuser.Text = "tên";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(53, 24);
+            this.toolStripLabel2.Text = "Xin chào";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnxoa);
+            this.groupBox1.Controls.Add(this.lblhdchon);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnsearch);
+            this.groupBox1.Controls.Add(this.txtsearch);
+            this.groupBox1.Controls.Add(this.datadshd);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.groupBox1.Location = new System.Drawing.Point(13, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 340);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh Sách Hóa Đơn";
+            // 
+            // datadshd
+            // 
+            this.datadshd.AllowUserToAddRows = false;
+            this.datadshd.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.datadshd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datadshd.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.datadshd.Location = new System.Drawing.Point(0, 79);
+            this.datadshd.Name = "datadshd";
+            this.datadshd.Size = new System.Drawing.Size(420, 255);
+            this.datadshd.TabIndex = 0;
+            this.datadshd.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datadshd_CellContentClick);
+            this.datadshd.SelectionChanged += new System.EventHandler(this.datadshd_SelectionChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "mahd";
+            this.Column1.HeaderText = "Số HD";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "nguoiban";
+            this.Column2.HeaderText = "Người Bán";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "khachmua";
+            this.Column3.HeaderText = "Sđt khách mua";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "ngaymua";
+            this.Column4.HeaderText = "Ngày Bán";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "tongtien";
+            this.Column5.HeaderText = "Thành tiền";
+            this.Column5.Name = "Column5";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbldoanhthu);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lbltonghd);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Turquoise;
+            this.groupBox2.Location = new System.Drawing.Point(456, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 339);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tổng kết";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tổng số HD";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbltonghd
+            // 
+            this.lbltonghd.AutoSize = true;
+            this.lbltonghd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltonghd.ForeColor = System.Drawing.Color.Crimson;
+            this.lbltonghd.Location = new System.Drawing.Point(145, 51);
+            this.lbltonghd.Name = "lbltonghd";
+            this.lbltonghd.Size = new System.Drawing.Size(92, 31);
+            this.lbltonghd.TabIndex = 2;
+            this.lbltonghd.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Doanh thu";
+            // 
+            // lbldoanhthu
+            // 
+            this.lbldoanhthu.AutoSize = true;
+            this.lbldoanhthu.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldoanhthu.ForeColor = System.Drawing.Color.Crimson;
+            this.lbldoanhthu.Location = new System.Drawing.Point(122, 120);
+            this.lbldoanhthu.Name = "lbldoanhthu";
+            this.lbldoanhthu.Size = new System.Drawing.Size(115, 39);
+            this.lbldoanhthu.TabIndex = 4;
+            this.lbldoanhthu.Text = "label4";
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Image = ((System.Drawing.Image)(resources.GetObject("btnsearch.Image")));
+            this.btnsearch.Location = new System.Drawing.Point(191, 21);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(34, 23);
+            this.btnsearch.TabIndex = 26;
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(6, 21);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(169, 22);
+            this.txtsearch.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "HD đang chọn";
+            // 
+            // lblhdchon
+            // 
+            this.lblhdchon.AutoSize = true;
+            this.lblhdchon.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhdchon.ForeColor = System.Drawing.Color.Crimson;
+            this.lblhdchon.Location = new System.Drawing.Point(118, 50);
+            this.lblhdchon.Name = "lblhdchon";
+            this.lblhdchon.Size = new System.Drawing.Size(66, 24);
+            this.lblhdchon.TabIndex = 28;
+            this.lblhdchon.Text = "label2";
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.ForeColor = System.Drawing.Color.Black;
+            this.btnxoa.Image = ((System.Drawing.Image)(resources.GetObject("btnxoa.Image")));
+            this.btnxoa.Location = new System.Drawing.Point(287, 39);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 37);
+            this.btnxoa.TabIndex = 29;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // FormQuanLyHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 382);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FormQuanLyHoaDon";
+            this.Text = "Quản Lý Hóa Đơn";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormQuanLyHoaDon_FormClosing);
+            this.Load += new System.EventHandler(this.FormQuanLyHoaDon_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datadshd)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnthoat;
+        private System.Windows.Forms.ToolStripLabel lbltenuser;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView datadshd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbldoanhthu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltonghd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Label lblhdchon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.TextBox txtsearch;
+    }
+}
---
+++ QuanLyNhaSach_DoAn/FormQuanLyHoaDon.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaSach_DoAn
+{
+    public partial class FormQuanLyHoaDon : Form
+    {
+        XuLy dt = new XuLy();
+        public FormQuanLyHoaDon()
+        {
+            InitializeComponent();
+        }
+        public string _textbox1
+        {
+            set { lbltenuser.Text = value; }
+        }
+
+        private void FormQuanLyHoaDon_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r;
+            r = MessageBox.Show("Bạn có muốn thoát?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (r == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void datadshd_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void datadshd_SelectionChanged(object sender, EventArgs e)
+        {
+            if (datadshd.CurrentCell != null)
+            {
+                btnxoa.Enabled = true;
+            }
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            datadshd.ClearSelection();
+            dt.SearchQLHD(datadshd, txtsearch.Text);
+        }
+
+        private void FormQuanLyHoaDon_Load(object sender, EventArgs e)
+        {
+            btnxoa.Enabled = false;
+            datadshd.DataSource = dt.LoadThemHD();
+            dt.DataBlindsHD(dt.LoadThemHD(), lblhdchon);
+            lbltonghd.Text = dt.tonghd(datadshd).ToString();
+            lbldoanhthu.Text = dt.tongdoanhthu(datadshd).ToString();
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            if (dt.XoaHD(datadshd, lblhdchon.Text) == true)
+            {
+                dt.LuuHD();
+                MessageBox.Show("Đã xóa hóa đơn");
+                lbltonghd.Text = dt.tonghd(datadshd).ToString();
+                lbldoanhthu.Text = dt.tongdoanhthu(datadshd).ToString();
+            }
+            
+        }
+    }
+}
